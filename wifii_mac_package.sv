a7407f92c9c7c68761ccd94a38b6f005ab02b118
Jomon K Joy <jomonkurianjoy@gmail.com>
2018-05-28 23:09:08 +0530

    Update wifii_mac_package.sv

--- wifii_mac_package.sv
+++ wifii_mac_package.sv
@@ -1,3 +1,17 @@
 package wifii_pkg;
+  
+  typedef struct {
+    logic [1:0] Protocol_Version;
+    logic [1:0] Type;
+    logic [1:0] Subtype;
+    logic to_DS;      // to the distributed system
+    logic from_DS;    // exit from distributed system
+    logic More_Frag;  // more fragmented frames to follow
+    logic Retry;      // re-transmission
+    logic Pwr_Mgt;    // station in power save mode
+    logic More_Data;  // additional frames buffered for the dest
+    logic WEP;        // data protected with WEP Algorithm
+    logic Order;      // frames must be strictly ordered
+  }frame_control_field;
 
 endpackage
